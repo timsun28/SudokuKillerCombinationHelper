fff10d7cefbda4ea34c299e48282295204e5c668
Timo Frionnet <tfrionnet@gmail.com>
2020-03-26 18:36:28 +1300

    Updates

---
+++ precache-manifest.f5328975e6c2a21a47fb6962e0e20312.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f657cfdbc7baab44d9b78bdbce86ccec",
+    "url": "/SudokuKillerCombinationHelper/index.html"
+  },
+  {
+    "revision": "4cdf01c722d533a1ad59",
+    "url": "/SudokuKillerCombinationHelper/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "9d8694ed8bb9d5281ae8",
+    "url": "/SudokuKillerCombinationHelper/static/js/2.260f8f98.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/SudokuKillerCombinationHelper/static/js/2.260f8f98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4cdf01c722d533a1ad59",
+    "url": "/SudokuKillerCombinationHelper/static/js/main.834951bc.chunk.js"
+  },
+  {
+    "revision": "f951d635d0d5540b9fda",
+    "url": "/SudokuKillerCombinationHelper/static/js/runtime-main.20494cb3.js"
+  }
+]);
